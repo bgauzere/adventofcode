465956b1eae241200af5c851018a2f492218bc93
Gauzere Benoit <benoit.gauzere@insa-rouen.fr>
2023-12-12 14:01:40 +0100

    jour 12 2023 part 2

--- 2023/day_12.py
+++ 2023/day_12.py
@@ -2,6 +2,8 @@ import numpy as np
 import sys
 from scipy.spatial.distance import cdist
 from itertools import combinations
+
+from functools import cache
 import re
 def read_content():
     file = sys.argv[1]
@@ -16,7 +18,7 @@ def check_sequences_ok(springs, sequences):
     ok = all([len(m) == sequence for m,sequence in zip(matches,sequences)])
     return ok
 
-if __name__ == "__main__":
+def first(): #brute force
     content = read_content()
     total_ok = 0
     for l in content:
@@ -47,3 +49,78 @@ if __name__ == "__main__":
         print(f"{number_ok = }")
         total_ok += number_ok
     print(f"{total_ok = }")
+
+def second():
+    content = read_content()
+    total_ok = 0
+    for l in content:
+        data_spring, sequences = l.split()
+        data_spring = (data_spring+"?")*5
+        data_spring = data_spring[:-1]
+        print(data_spring)
+        sequences = tuple([int(s) for s in sequences.split(",")])
+        sequences = sequences*5
+        print(sequences)
+        cur_ok = nb_ok(data_spring+".", sequences)
+        print(f"{cur_ok = }")
+        total_ok += cur_ok
+    print(f"{total_ok = }")
+
+@cache
+def nb_ok(springs, sequences,serie_en_cours=False):
+    #print(f"{springs = }, {sequences = }, {sequence = }")
+    #print(f"not(springs) = {not(springs)}")
+    nb_springs = sum([x=="#" for x in springs])
+    nb_marks = sum([x=="?" for x in springs])
+    if not(springs):
+        if sum(sequences) ==0:
+            print(f"Found a solution (1) : {springs = }, {sequences = }")
+        return sum(sequences) == 0 #nb_springs == 0
+    if not(sequences):
+        if set(springs) == set("."):
+            print(f"Found a solution (2): {springs = }, {sequences = }")
+        return not "#" in springs
+    
+    if not "#" in springs:#  and sum(sequences) == 0: #nb_springs == 0
+        #print(f"Found a solution: {springs = }, {sequences = }")
+        if sum(sequences) == 0:
+            #print(f"Found a solution (3): {springs = }, {sequences = }, {sequence = }")
+            return 1
+    if sum(sequences) == 0 and "#" in springs:
+        return 0
+    
+    cur_nb_solutions = 0
+    next = springs[0]
+    
+    if next == "?":
+        possibilities = ["#", "."]
+    else:
+        possibilities = [next]
+
+    for next in possibilities:
+        #print(f"{next = }")
+        #cur_sequence = sequence + next
+        if next == "#":
+            if sequences[0] == 0:
+                pass #return 0
+            else:
+                cur_sequences = tuple([sequences[0]-1 , *sequences[1:]])#.copy()
+                #cur_sequences[0] -= 1
+                #print(f"{next=}")
+                cur_nb_solutions += nb_ok(springs[1:], cur_sequences,
+                                          serie_en_cours=True)
+        else: # next == "."
+            if serie_en_cours and sequences[0] == 0: # on l'a terminé, on passe à la suivante
+                if len(sequences)>1:
+                    #print(f"{next =}")
+                    cur_nb_solutions += nb_ok(springs[1:], sequences[1:],
+                                              serie_en_cours=False) 
+            elif serie_en_cours and sequences[0] > 0:
+                pass # on stop, on a cassé la liste
+            else:
+                #print(f"{next}")
+                cur_nb_solutions += nb_ok(springs[1:], sequences,
+                                          serie_en_cours=False) 
+    return cur_nb_solutions
+if __name__ == "__main__":
+    second()
