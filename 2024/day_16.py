bddbd7618ef43bb882ea3175f22a9f8bb9672075
Gauzere Benoit <benoit.gauzere@insa-rouen.fr>
2024-12-17 23:24:25 +0100

    jour 16 et 17 2024

---
+++ 2024/day_16.py
@@ -0,0 +1,137 @@
+import sys
+import networkx as nx
+import matplotlib.pyplot as plt
+from collections import defaultdict 
+import heapq
+
+import matplotlib.pyplot as plt
+
+def read_content(file):
+    lab = nx.DiGraph()
+    with open(file, "r") as f:
+        content = f.readlines()
+        for i,l in enumerate(content):
+            if "S" in l:
+                start = (i,l.index("S"))
+                content[i] = content[i].replace("S",".")
+            if "E" in l:
+                end = (i,l.index("E"))
+                content[i] = content[i].replace("E",".")
+        height = len(content)
+        for i in range(1,len(content)-1):
+            line = content[i].strip()
+            #print(f"--{line}--")
+            if "S" in line:
+                start = (i,line.index("S"))
+            for j in range(1,len(line)-1):
+                width = len(line)
+                if content[i][j] == ".":
+                    lab.add_node((i,j))
+                    # check for neighbours:
+                    if content[i-1][j] == ".":
+                        lab.add_edge((i,j), (i-1,j), dir=1)
+                    if content[i+1][j] == ".":
+                        lab.add_edge((i,j), (i+1,j), dir=3)
+                    if content[i][j-1] == ".":
+                        lab.add_edge((i,j), (i,j-1), dir=2)
+                    if content[i][j+1] == ".":
+                        lab.add_edge((i,j), (i,j+1), dir=0)
+    return lab, start, end, height, width
+
+def next_dir(dir):
+    return (dir+1)%4
+
+def diff_dir(cur_dir,next_dir):
+    return abs(next_dir - cur_dir)%2
+
+def shortest_path(lab, start, end):
+    distances = {(node,dir) : float("inf") for node in lab.nodes for dir in range(4)}
+    distances[(start,0)] = 0
+    priority_queue = [(0, start,0)]
+    
+    map_directions = {node:"." for node in lab.nodes}
+    pred = {}
+    while priority_queue:
+        cur_dist, cur_node, cur_dir = heapq.heappop(priority_queue)
+        #print(f"cur_node {cur_node} cur_dist {cur_dist}, {distances[cur_node,cur_dir]}")
+        if cur_node == end:
+            print(f"Shortest path is {distances[end,cur_dir]}, {cur_dist}")
+            return distances, map_directions, pred
+        if cur_dist > distances[cur_node,cur_dir]:
+            continue
+        for next_node in lab.neighbors(cur_node):
+            next_dir = lab[cur_node][next_node]["dir"]  # Get the direction of the next edge
+            turn_cost = diff_dir(cur_dir, next_dir) * 1000  # Compute the cost of turning
+            new_distance = cur_dist + turn_cost + 1  # Add movement cost
+            if new_distance < distances[next_node,next_dir]:  # Update if we found a shorter path
+                distances[next_node,next_dir] = new_distance
+                map_directions[next_node] = next_dir  # Save the direction
+                pred[next_node, (next_dir+2)%2] = cur_node
+                heapq.heappush(priority_queue, (new_distance, next_node, next_dir))  # Push the updated direction
+    return distances, map_directions, pred
+
+
+def dir_to_str(dir):
+    if dir == 0:
+        return ">"
+    if dir == 1:
+        return "^"
+    if dir == 2:
+        return "<"
+    if dir == 3:
+        return "v"
+    
+def display(lab, map_directions,pred, end, start, height, width):
+    print(map_directions)
+    
+    map_to_display = []
+    for i in range(height):
+        line = []
+        for j in range(width):
+            if (i,j) in lab.nodes:
+                line.append(".")
+            else:
+                line.append("#")
+        map_to_display.append(line)
+    for k in map_directions:
+        if map_directions[k] != ".":
+            map_to_display[k[0]][k[1]] = "*"
+    # to_explore = {(end, 5)}
+    # explored = set()
+    # while to_explore:
+    #     print(to_explore)
+    #     cur_node,cur_d = to_explore.pop()
+    #     explored.add((cur_node,cur_d))
+    #     #if != start:
+    #     map_to_display[cur_node[0]][cur_node[1]] = "*" #dir_to_str(map_directions[cur_node])
+    #     for d in range(4):
+    #         if (cur_node,d) in pred:
+    #             next_node = pred[cur_node,d]
+    #             if (next_node,d) not in explored:
+    #                 to_explore.add((next_node,d))
+    
+    for i in range(height):
+        print("".join(map_to_display[i]))
+    # plt.matshow(map_to_display)
+    # plt.savefig("maze.svg")
+    # plt.show()
+
+
+if __name__ == "__main__":
+    lab,start, end, height, width= read_content(sys.argv[1])
+    # nx.draw(lab, with_labels=True)
+    # plt.show()
+    cur_node = start
+    cur_dir = 0
+    result, directions, pred = shortest_path(lab, start, end)
+    for dir in range(4):
+        print(result[end,dir])
+    #print(result)
+    # dijsktra in python  on lab without networkx
+    display(lab, directions, pred, end, start,height, width)
+
+
+
+
+
+    
---
+++ 2024/day_17.py
@@ -0,0 +1,154 @@
+import sys 
+
+class Prog:
+    def __init__(self, A,B,C, code):
+        self.A = A
+        self.B = B
+        self.C = C
+
+        self.opcode = []
+        self.operands = []
+
+        data  = [int(opcode) for opcode in code.split(",")]
+        self.code = data
+        # for i in range(0,len(data),2):
+        #     self.opcode.append(data[i])
+        #     self.operands.append(data[i+1])
+
+        self.i = 0
+
+        self.output = []
+
+    def get_operand(self, operand):
+        if operand <= 3:
+            return operand
+        if operand == 4:
+            return self.A
+        if operand == 5:
+            return self.B
+        if operand == 6:
+            return self.C
+        return None
+
+    
+    def exec(self):
+        if self.i >= len(self.code):
+            return False
+        opcode = self.code[self.i]
+        operand = self.code[self.i+1]
+        combo_operand = self.get_operand(operand)
+
+        if opcode == 0:
+
+            # the adv instruction (opcode 0) performs division.
+            #  The numerator is the value in the A register.
+            #  The denominator is found by raising 2 to the power of the instruction's combo operand. 
+            #  (So, an operand of 2 would divide A by 4 (2^2); 
+            #  an operand of 5 would divide A by 2^B.) 
+            # The result of the division operation is truncated to an integer
+            #  and then written to the A register.
+            self.A = self.A // (2**combo_operand)
+        if opcode == 1:
+            # The bxl instruction (opcode 1) calculates the bitwise XOR of 
+            # register B and the instruction's literal operand,
+            # then stores the result in register B.
+            self.B = self.B ^ operand
+        if opcode == 2:
+            # The bst instruction (opcode 2) calculates the value of its combo
+            #  operand modulo 8 (thereby keeping only its lowest 3 bits), 
+            # then writes that value to the B register.
+            self.B = combo_operand % 8
+        if opcode == 3:
+            # The jnz instruction (opcode 3) does nothing if the A register is 0.
+            #  However, if the A register is not zero, it jumps by setting the 
+            # instruction pointer to the value of its literal operand; 
+            # if this instruction jumps, the instruction pointer is not increased 
+            # by 2 after this instruction.
+            if self.A != 0:
+                self.i = operand - 2
+        if opcode == 4:
+            # The bxc instruction (opcode 4) calculates the bitwise XOR
+            #  of register B and register C, then stores the result in register B.
+            #  (For legacy reasons, this instruction reads an operand but ignores it.)
+            self.B = self.B ^ self.C
+        if opcode == 5:
+            # The out instruction (opcode 5) calculates the value of its combo 
+            # operand modulo 8, then outputs that value.
+            # (If a program outputs multiple values, they are separated by commas.)
+            self.output.append(combo_operand % 8)
+        if opcode == 6:
+            # The bdv instruction (opcode 6) works exactly like the adv instruction
+            #  except that the result is stored in the B register.
+            #  (The numerator is still read from the A register.)
+            self.B = self.A // (2**combo_operand)
+        if opcode == 7:
+            # The cdv instruction (opcode 7) works exactly like the adv instruction
+            #  except that the result is stored in the C register. 
+            # (The numerator is still read from the A register.)
+            self.C = self.A // (2**combo_operand)
+        self.i += 2
+        return True
+    
+    def __str__(self):
+        return ",".join([str(n) for n in self.output])
+
+def test():
+    # If register C contains 9, the program 2,6 would set register B to 1.
+    t1 = Prog(0,0,9,"2,6")
+    t1.exec()
+    assert(t1.B == 1)
+    print("Test 1 OK")
+
+    # If register A contains 10, the program 5,0,5,1,5,4 would output 0,1,2.
+    t2 = Prog(10,0,0,"5,0,5,1,5,4")
+    while(t2.exec()):
+        pass
+    assert(str(t2) == "0,1,2")
+    print("Test 2 OK")    
+    # If register A contains 2024, the program 0,1,5,4,3,0 would output 4,2,5,6,7,7,7,7,3,1,0 and leave 0 in register A.
+    t3 = Prog(2024,0,0,"0,1,5,4,3,0")
+    while(t3.exec()):
+        pass
+    assert(str(t3) == "4,2,5,6,7,7,7,7,3,1,0")
+    assert(t3.A == 0)
+    print("Test 3 OK")
+    # If register B contains 29, the program 1,7 would set register B to 26.
+    t4 = Prog(0,29,0,"1,7")
+    t4.exec()
+    assert(t4.B == 26)
+    print("Test 4 OK")
+    # If register B contains 2024 and register C contains 43690, the program 4,0 would set register B to 44354.
+    t5 = Prog(0,2024,43690,"4,0")
+    t5.exec()
+    assert(t5.B == 44354)
+    print("Test 5 OK")
+
+
+def get_prog(filename):
+    with open(filename) as f:
+        data = f.readlines()
+        A = int(data[0].split(" ")[2])
+        B = int(data[1].split(" ")[2])
+        C = int(data[2].split(" ")[2])
+        code = data[4].split(" ")[1]  
+        return Prog(A,B,C,code)
+
+if __name__ == "__main__":
+    prog = get_prog(sys.argv[1])
+    while(prog.exec()):
+        pass
+    print(str(prog))
+
+    target = ",".join([str(n) for n in prog.code])
+    print(target)
+    value = 0
+    prog = Prog(value,0,0,target)
+    while(prog.exec()):
+        pass
+        
+    while str(prog) != target:
+        value += 1
+        prog = Prog(value,0,0,target)
+        while(prog.exec()):
+            pass
+        print(value)
