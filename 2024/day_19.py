999623d52f10c8175c1b24a48c54cfbf23b3fdde
Gauzere Benoit <benoit.gauzere@insa-rouen.fr>
2025-01-08 21:24:05 +0100

    day 19 part 2

--- 2024/day_19.py
+++ 2024/day_19.py
@@ -1,4 +1,8 @@
 import sys
+from tqdm import tqdm
+
+from functools import lru_cache
+
 
 def read_content(filename):
     with open(filename) as f:
@@ -7,28 +11,56 @@ def read_content(filename):
         towels = []
         for towel in data[2:]:
             towels.append(towel.strip())
-        return patterns, towels
-    
+        return tuple(patterns), tuple(towels)
+
+
+@lru_cache(maxsize=None)
+def find_all_patterns(patterns, towel, nb_patterns=0):
+    if len(towel) == 0:
+        # print("Towel is empty")
+        return 1
+    cur_nb = 0
+    for pattern in patterns:
+        # print(f"Pattern : --{pattern}--")
+        if towel.startswith(pattern):
+            # print(towel, pattern)
+            # print(f"Pattern found : {pattern}")
+            cur_nb += find_all_patterns(
+                patterns, towel[len(pattern) :], nb_patterns
+            )
+
+    return nb_patterns + cur_nb
+
+
 def find_pattern(patterns, towel):
     if len(towel) == 0:
         return True
     for pattern in patterns:
-        #print(f"Pattern : --{pattern}--")
+        # print(f"Pattern : --{pattern}--")
         if towel.startswith(pattern):
-            #print(f"Pattern found : {pattern}")
-            if find_pattern(patterns, towel[len(pattern):]):
+            # print(f"Pattern found : {pattern}")
+            if find_pattern(patterns, towel[len(pattern) :]):
                 return True
     return False
 
+
 if __name__ == "__main__":
     patterns, towels = read_content(sys.argv[1])
-    #print(f"Towel : --{towels[0]}--")
-    #find_pattern(patterns, towels[0])
+    # print(f"Towel : --{towels[0]}--")
+    # find_pattern(patterns, towels[0])
     nb_possible = 0
     for towel in towels:
         if find_pattern(patterns, towel):
-            print(f"{towel} is possible")
+            # print(f"{towel} is possible")
             nb_possible += 1
         else:
-            print(f"{towel} is impossible")
+            pass
+            # print(f"{towel} is impossible")
     print(nb_possible)
+
+    ### Part 2
+    sum_up = 0
+    for towel in tqdm(towels):
+        nb_patterns = find_all_patterns(patterns, towel)
+        sum_up += nb_patterns
+    print(sum_up)
